e6f8b857c11d020dd632c31415553eaa0644336b
StiphyJay <601772137@qq.com>
2024-01-03 11:36:45 +0800

    simplify visualize script

--- tools/visualize.py
+++ tools/visualize.py
@@ -1,59 +1,12 @@
 import argparse
-import glob
 from pathlib import Path
 import mayavi.mlab as mlab
 from visual_utils import visualize_utils as V
 OPEN3D_FLAG = True
-import pickle
 import kitti_util as utils
 import numpy as np
-import torch
 import os
 import pdb
-from pcdet.config import cfg, cfg_from_yaml_file
-from pcdet.datasets import DatasetTemplate
-from pcdet.models import build_network, load_data_to_gpu
-from pcdet.utils import common_utils
-
-
-class DemoDataset(DatasetTemplate):
-    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None, ext='.bin'):
-        """
-        Args:
-            root_path:
-            dataset_cfg:
-            class_names:
-            training:
-            logger:
-        """
-        super().__init__(
-            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger
-        )
-        self.root_path = root_path
-        self.ext = ext
-        data_file_list = glob.glob(str(root_path / f'*{self.ext}')) if self.root_path.is_dir() else [self.root_path]
-
-        data_file_list.sort()
-        self.sample_file_list = data_file_list
-
-    def __len__(self):
-        return len(self.sample_file_list)
-
-    def __getitem__(self, index):
-        if self.ext == '.bin':
-            points = np.fromfile(self.sample_file_list[index], dtype=np.float32).reshape(-1, 4)
-        elif self.ext == '.npy':
-            points = np.load(self.sample_file_list[index])
-        else:
-            raise NotImplementedError
-
-        input_dict = {
-            'points': points,
-            'frame_id': index,
-        }
-
-        data_dict = self.prepare_data(data_dict=input_dict)
-        return data_dict
     
 class kitti_object(object):
     """Load and parse object data into a usable format."""
@@ -170,8 +123,6 @@ def parse_config():
 
 def main():
     args = parse_config()
-    logger = common_utils.create_logger()
-    logger.info('-----------------Quick Demo of OpenPCDet-------------------------')
     pred_path = '/home/zhousifan/OpenPCDet/output/home/zhousifan/OpenPCDet/tools/cfgs/kitti_models/HA_pointpillar_64_128o_fixs/default/eval/epoch_80/val/default/final_result/data'
     dataset = kitti_object(pred_path, args=args)
     for data_idx in range(len(dataset)):
